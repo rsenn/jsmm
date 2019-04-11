a90b5288c7f6ab90bfd77708e5a0d7edb92f039e
Roman Senn <roman.l.senn@gmail.com>
2019-04-11 08:25:16 +0200

    jsmm c++ fixes

--- jsmm/src/stringapi.cpp
+++ jsmm/src/stringapi.cpp
@@ -17,6 +17,9 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 
+#include <cstdlib>
+#include <cstring>
+
 #include <jsmm/stringapi>
 
 #include <js/jsapi.h>
